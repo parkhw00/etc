19d14f6aa6231706bfb9e33a7669857bed058436
Hyunwoo Park <hyunwoo.park@gmail.com>
2018-07-09 16:08:55 +0000

    matrix: load image

--- RGBMatrixGPIO/RGBMatrixGPIO.c
+++ RGBMatrixGPIO/RGBMatrixGPIO.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
 
 #define debug(fmt,args...)	printf(fmt, ##args)
 #define info(fmt,args...)	printf(fmt, ##args)
@@ -66,7 +67,7 @@ void *map (unsigned long addr, int size)
 unsigned int RD_CFG (unsigned int A, unsigned int b)
 {
 	unsigned int val;
-	
+
 	val = *(volatile unsigned int *)(reg_base + CFG(A, b));
 
 	return (val >> (b & 0x7) * 4) & 0xf;
@@ -75,7 +76,7 @@ unsigned int RD_CFG (unsigned int A, unsigned int b)
 unsigned int WR_CFG (unsigned int A, unsigned int b, unsigned int val)
 {
 	unsigned int tmp;
-	
+
 	tmp = *(volatile unsigned int *)(reg_base + CFG(A, b));
 
 	tmp &= ~(0x7U << (b&0x7)*4);
@@ -89,7 +90,7 @@ unsigned int WR_CFG (unsigned int A, unsigned int b, unsigned int val)
 unsigned int RD_PUL (unsigned int A, unsigned int b)
 {
 	unsigned int val;
-	
+
 	val = *(volatile unsigned int *)(reg_base + PUL(A, b));
 
 	return (val >> (b & 0xf) * 2) & 0x3;
@@ -98,7 +99,7 @@ unsigned int RD_PUL (unsigned int A, unsigned int b)
 unsigned int WR_PUL (unsigned int A, unsigned int b, unsigned int val)
 {
 	unsigned int tmp;
-	
+
 	tmp = *(volatile unsigned int *)(reg_base + PUL(A, b));
 
 	tmp &= ~(0x3U << (b&0xf)*2);
@@ -143,7 +144,7 @@ int dump (void)
 }
 
 /* return in microsecond */
-int get_time (void)
+static int get_time (void)
 {
 	struct timeval tv;
 
@@ -153,44 +154,73 @@ int get_time (void)
 }
 
 #define CBITS	4
-int set_line (int row, int bit, int frame_count)
+unsigned char scanbuf[16][CBITS][64];
+
+unsigned char *fb_data;
+
+/* fb, ARGB 8888 format */
+int convert_to_scanbuf (const unsigned char *fb)
 {
-	int ani = frame_count / 2;
-	int i;
-	unsigned int out;
+	int W = 64;
+	int H = 32;
+	int stride = W * 4;
 
-	for (i=0; i<64; i++)
+	int row, bit, col;
+
+	for (row=0; row<16; row++)
 	{
-		unsigned int rgb;
-		unsigned int c, b;
-
-		rgb = 0;
-
-		c = ani + i - row;
-		b = ani + i + row;
-		if (b & (0x40 >> (CBITS-bit)))
+		for (col=0; col<64; col++)
 		{
-			if (((c + 0) % 16) == 0) rgb |= 0x01;
-			if (((c + 1) % 16) == 0) rgb |= 0x02;
-			if (((c + 2) % 16) == 0) rgb |= 0x04;
-			if (((c + 3) % 16) == 0) rgb |= 0x07;
-		}
+			unsigned char r0, g0, b0;
+			unsigned char r1, g1, b1;
 
-		c = ani + i - (row + 16);
-		b = ani + i + (row + 16);
-		if (b & (0x40 >> (CBITS-bit)))
-		{
-			if (((c + 0) % 16) == 0) rgb |= 0x08;
-			if (((c + 1) % 16) == 0) rgb |= 0x10;
-			if (((c + 2) % 16) == 0) rgb |= 0x20;
-			if (((c + 3) % 16) == 0) rgb |= 0x38;
-		}
+#if 1
+			r0 = *(fb+(64*(row+ 0)+col)*4 + 2);
+			g0 = *(fb+(64*(row+ 0)+col)*4 + 1);
+			b0 = *(fb+(64*(row+ 0)+col)*4 + 0);
 
-		out = orgA | (rgb << BIT_RGB);
-		DAT ('A') = out;
-		DAT ('A') = out | (1U << BIT_CLK);
+			r1 = *(fb+(64*(row+16)+col)*4 + 2);
+			g1 = *(fb+(64*(row+16)+col)*4 + 1);
+			b1 = *(fb+(64*(row+16)+col)*4 + 0);
+#else
+			r0 = g0 = b0 = 0;
+			r1 = g1 = b1 = 0;
+			if (((row - col) % 10) == 0)
+				r0 = 0x80;
+			if ((((row+16) - col) % 10) == 0)
+				r1 = 0x10;
+
+			if (((row - col) % 12) == 0)
+				g0 = 0x40;
+			if ((((row+16) - col) % 12) == 0)
+				g1 = 0x10;
+
+			if (((row - col) % 14) == 0)
+				b0 = 0x20;
+			if ((((row+16) - col) % 14) == 0)
+				b1 = 0x10;
+#endif
+
+			for (bit=0; bit<4; bit++)
+			{
+				unsigned int out;
+
+				out = 0;
+				if (r0 & (0x80>>(CBITS-1-bit))) out |= 0x01;
+				if (g0 & (0x80>>(CBITS-1-bit))) out |= 0x02;
+				if (b0 & (0x80>>(CBITS-1-bit))) out |= 0x04;
+				if (r1 & (0x80>>(CBITS-1-bit))) out |= 0x08;
+				if (g1 & (0x80>>(CBITS-1-bit))) out |= 0x10;
+				if (b1 & (0x80>>(CBITS-1-bit))) out |= 0x20;
+
+				scanbuf[row][bit][col] = out;
+			}
+		}
 	}
+}
 
+static int display_line (int row)
+{
 	DAT ('A') = orgA | (1U << BIT_OE);
 	DAT ('G') = orgG | (row << BIT_ABCD);
 
@@ -200,24 +230,38 @@ int set_line (int row, int bit, int frame_count)
 	return 0;
 }
 
+static int send_line (int row, int bit)
+{
+	int i;
+
+	unsigned char *buf = scanbuf[row][bit];
+
+	for (i=0; i<64; i++)
+	{
+		unsigned int out;
+
+		out = orgA | (buf[i] << BIT_RGB);
+		DAT ('A') = out;
+		DAT ('A') = out | (1U << BIT_CLK);
+	}
+
+	return 0;
+}
+
 int got_sig;
+#define HZ	60
 
 #define ARRAY_SIZE(arr)		(sizeof(arr)/sizeof(arr[0]))
-int loop_scan (void)
+static int loop_scan (void)
 {
 	static const int on_tab[CBITS] =
 	{
-#if 0
-		100,
-		200,
-		400,
-		800,
-#else
-		 80,
-		160,
-		320,
-		640,
-#endif
+#define UNIT_DUR_HZ(n,HZ)	(n*1000000/HZ/16/((1<<CBITS)-1))
+#define UNIT_DUR(n)		UNIT_DUR_HZ(n,HZ)
+		UNIT_DUR(1),
+		UNIT_DUR(2),
+		UNIT_DUR(4),
+		UNIT_DUR(8),
 	};
 
 	int frame_count;
@@ -236,10 +280,41 @@ int loop_scan (void)
 
 		/* scan a row */
 		prev = next;
-		set_line (row, bit, frame_count);
+		display_line (row);
 
 		/* calculate next time, wait until next scan time */
 		next += on_tab[bit];
+
+		bit ++;
+		if (bit >= CBITS)
+		{
+			bit = 0;
+			row ++;
+			if (row >= 16)
+			{
+				row = 0;
+				frame_count ++;
+				//printf ("%9dus, frame %d\n", now, frame_count);
+
+				{
+					int yoff;
+
+					yoff = 1.*(128.-32.)*(1. - cos(2.*M_PI*0.25*frame_count/HZ))/2;
+
+					if (yoff > 128-32)
+						yoff = 128-32;
+					if (yoff < 0)
+						yoff = 0;
+
+					//printf ("frame %d, yoff %d\n", frame_count, yoff);
+
+					convert_to_scanbuf (fb_data + yoff*64*4);
+				}
+			}
+		}
+
+		send_line (row, bit);
+
 		last_process = -1;
 		while (1)
 		{
@@ -259,19 +334,70 @@ int loop_scan (void)
 				break;
 			}
 		}
+	}
+}
 
-		bit ++;
-		if (bit >= CBITS)
+int load_fb (const char *name)
+{
+#if 1
+	int fd, size;
+
+	fd = open (name, O_RDONLY);
+	if (fd < 0)
+		fatal ("cannot open %s\n", name);
+
+	size = lseek (fd, 0, SEEK_END);
+	lseek (fd, 0, SEEK_SET);
+
+	fb_data = malloc (size);
+	if (read (fd, fb_data, size) < 0)
+		fatal ("read() failed.\n");
+
+	convert_to_scanbuf (fb_data);
+#else
+	int i, j;
+
+	fb_data = malloc (64*128*4);
+
+	for (j=0; j<128; j++)
+	{
+		for (i=0; i<64; i++)
 		{
-			bit = 0;
-			row ++;
-			if (row >= 16)
+			if (i < 8)
 			{
-				row = 0;
-				frame_count ++;
+				*(fb_data + (j*64 + i)*4 + 0) = j*2;
+				*(fb_data + (j*64 + i)*4 + 1) = j*2;
+				*(fb_data + (j*64 + i)*4 + 2) = j*2;
+			}
+			else if (i < 16)
+			{
+				*(fb_data + (j*64 + i)*4 + 0) = 0;
+				*(fb_data + (j*64 + i)*4 + 1) = 0;
+				*(fb_data + (j*64 + i)*4 + 2) = j*2;
+			}
+			else if (i < 24)
+			{
+				*(fb_data + (j*64 + i)*4 + 0) = 0;
+				*(fb_data + (j*64 + i)*4 + 1) = j*2;
+				*(fb_data + (j*64 + i)*4 + 2) = 0;
+			}
+			else if (i < 32)
+			{
+				*(fb_data + (j*64 + i)*4 + 0) = j*2;
+				*(fb_data + (j*64 + i)*4 + 1) = 0;
+				*(fb_data + (j*64 + i)*4 + 2) = 0;
+			}
+			else
+			{
+				*(fb_data + (j*64 + i)*4 + 0) = 0;
+				*(fb_data + (j*64 + i)*4 + 1) = 0;
+				*(fb_data + (j*64 + i)*4 + 2) = 0;
 			}
 		}
 	}
+#endif
+
+	return 0;
 }
 
 void sighandle (int num)
@@ -289,6 +415,8 @@ int main (int argc, char **argv)
 	reg_base = map (GPIO_BASE, GPIO_SIZE);
 
 	info ("gpio base %p\n", reg_base);
+
+	load_fb (argv[1]);
 
 	//dump ();
 

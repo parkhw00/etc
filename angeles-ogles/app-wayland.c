58e67b49ce6db2cc749e4cc20302668e919960c8
Hyunwoo Park <hyunwoo.park@gmail.com>
2021-03-22 23:51:29 +0900

    angeles-ogles: add wayland interface

---
+++ angeles-ogles/app-wayland.c
@@ -0,0 +1,405 @@
+/* San Angeles Observation OpenGL ES version example
+ * Copyright 2004-2005 Jetro Lauha
+ * All rights reserved.
+ * Web: http://iki.fi/jetro/
+ *
+ * This source is free software; you can redistribute it and/or
+ * modify it under the terms of EITHER:
+ *   (1) The GNU Lesser General Public License as published by the Free
+ *       Software Foundation; either version 2.1 of the License, or (at
+ *       your option) any later version. The text of the GNU Lesser
+ *       General Public License is included with this source in the
+ *       file LICENSE-LGPL.txt.
+ *   (2) The BSD-style license that is included with this source in
+ *       the file LICENSE-BSD.txt.
+ *
+ * This source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files
+ * LICENSE-LGPL.txt and LICENSE-BSD.txt for more details.
+ *
+ * $Id: app-linux.c,v 1.4 2005/02/08 18:42:48 tonic Exp $
+ * $Revision: 1.4 $
+ *
+ * Parts of this source file is based on test/example code from
+ * GLESonGL implementation by David Blythe. Here is copy of the
+ * license notice from that source:
+ *
+ * Copyright (C) 2003  David Blythe   All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * DAVID BLYTHE BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include <wayland-client.h>
+#include <wayland-server.h>
+#include <wayland-client-protocol.h>
+#include <wayland-egl.h>
+
+#include <EGL/egl.h>
+#include <EGL/eglplatform.h>
+
+#include <GLES/gl.h>
+#include <GLES/egl.h>
+
+#include "app.h"
+
+#define LOG(f,a...)	fprintf(stderr,"%s.%d "f,__func__,__LINE__,##a)
+
+struct wl_compositor *compositor = NULL;
+struct wl_surface *surface;
+struct wl_egl_window *egl_window;
+struct wl_region *region;
+struct wl_shell *shell;
+struct wl_shell_surface *shell_surface;
+
+struct _escontext
+{
+  /// Native System informations
+  EGLNativeDisplayType native_display;
+  EGLNativeWindowType native_window;
+  uint16_t window_width, window_height;
+  /// EGL display
+  EGLDisplay  display;
+  /// EGL context
+  EGLContext  context;
+  /// EGL surface
+  EGLSurface  surface;
+
+} ESContext = {
+  .native_display = NULL,
+  .window_width = 0,
+  .window_height = 0,
+  .native_window  = 0,
+  .display = NULL,
+  .context = NULL,
+  .surface = NULL
+};
+
+int gAppAlive = 1;
+
+static char sAppName[] =
+    "San Angeles Observation OpenGL ES version example (Linux)";
+static int sWindowWidth = WINDOW_DEFAULT_WIDTH;
+static int sWindowHeight = WINDOW_DEFAULT_HEIGHT;
+static EGLDisplay sEglDisplay = EGL_NO_DISPLAY;
+static EGLConfig sEglConfig;
+static EGLContext sEglContext = EGL_NO_CONTEXT;
+static EGLSurface sEglSurface = EGL_NO_SURFACE;
+
+
+#define CASE_STR( value ) case value: return #value;
+const char* glGetErrorString( GLenum error )
+{
+    switch( error )
+    {
+    CASE_STR( GL_NO_ERROR             )
+    CASE_STR( GL_INVALID_ENUM         )
+    CASE_STR( GL_INVALID_VALUE        )
+    CASE_STR( GL_INVALID_OPERATION    )
+    CASE_STR( GL_STACK_OVERFLOW       )
+    CASE_STR( GL_STACK_UNDERFLOW      )
+    CASE_STR( GL_OUT_OF_MEMORY        )
+    default: return "Unknown";
+    }
+}
+const char* eglGetErrorString( EGLint error )
+{
+    switch( error )
+    {
+    CASE_STR( EGL_SUCCESS             )
+    CASE_STR( EGL_NOT_INITIALIZED     )
+    CASE_STR( EGL_BAD_ACCESS          )
+    CASE_STR( EGL_BAD_ALLOC           )
+    CASE_STR( EGL_BAD_ATTRIBUTE       )
+    CASE_STR( EGL_BAD_CONTEXT         )
+    CASE_STR( EGL_BAD_CONFIG          )
+    CASE_STR( EGL_BAD_CURRENT_SURFACE )
+    CASE_STR( EGL_BAD_DISPLAY         )
+    CASE_STR( EGL_BAD_SURFACE         )
+    CASE_STR( EGL_BAD_MATCH           )
+    CASE_STR( EGL_BAD_PARAMETER       )
+    CASE_STR( EGL_BAD_NATIVE_PIXMAP   )
+    CASE_STR( EGL_BAD_NATIVE_WINDOW   )
+    CASE_STR( EGL_CONTEXT_LOST        )
+    default: return "Unknown";
+    }
+}
+#undef CASE_STR
+
+#define checkGLErrors()         _checkGLErrors(__func__, __LINE__)
+static void _checkGLErrors(const char *func, int line)
+{
+    GLenum error = glGetError();
+    if (error != GL_NO_ERROR)
+    {
+        fprintf(stderr, "GL Error: 0x%04x(%s) at %s.%d\n", (int)error, glGetErrorString(error), func, line);
+        exit (1);
+    }
+}
+
+#define checkEGLErrors()        _checkEGLErrors(__func__, __LINE__)
+static void _checkEGLErrors(const char *func, int line)
+{
+    EGLint error = eglGetError();
+    // GLESonGL seems to be returning 0 when there is no errors?
+    if (error && error != EGL_SUCCESS)
+    {
+        fprintf(stderr, "EGL Error: 0x%04x(%s) at %s.%d\n", (int)error, eglGetErrorString(error), func, line);
+        exit (1);
+    }
+}
+
+void CreateNativeWindow(char *title, int width, int height) {
+
+  region = wl_compositor_create_region(compositor);
+
+  wl_region_add(region, 0, 0, width, height);
+  wl_surface_set_opaque_region(surface, region);
+
+  struct wl_egl_window *egl_window =
+    wl_egl_window_create(surface, width, height);
+
+  if (egl_window == EGL_NO_SURFACE) {
+    LOG("No window !?\n");
+    exit(1);
+  }
+  else LOG("Window created !\n");
+  ESContext.window_width = width;
+  ESContext.window_height = height;
+  ESContext.native_window = egl_window;
+
+}
+
+EGLBoolean CreateEGLContext ()
+{
+   EGLint numConfigs;
+   EGLint majorVersion;
+   EGLint minorVersion;
+   EGLContext context;
+   EGLSurface surface;
+   EGLConfig config;
+   EGLint fbAttribs[] =
+   {
+       EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+       //EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+       EGL_RED_SIZE,        8,
+       EGL_GREEN_SIZE,      8,
+       EGL_BLUE_SIZE,       8,
+       EGL_NONE
+   };
+   EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE, EGL_NONE };
+   EGLDisplay display = eglGetDisplay( ESContext.native_display );
+   if ( display == EGL_NO_DISPLAY )
+   {
+      LOG("No EGL Display...\n");
+      return EGL_FALSE;
+   }
+
+   // Initialize EGL
+   if ( !eglInitialize(display, &majorVersion, &minorVersion) )
+   {
+      LOG("No Initialisation...\n");
+      return EGL_FALSE;
+   }
+
+   // Get configs
+   if ( (eglGetConfigs(display, NULL, 0, &numConfigs) != EGL_TRUE) || (numConfigs == 0))
+   {
+      LOG("No configuration...\n");
+      return EGL_FALSE;
+   }
+
+   // Choose config
+   if ( (eglChooseConfig(display, fbAttribs, &config, 1, &numConfigs) != EGL_TRUE) || (numConfigs != 1))
+   {
+      LOG("No configuration...\n");
+      return EGL_FALSE;
+   }
+
+   // Create a surface
+   surface = eglCreateWindowSurface(display, config, ESContext.native_window, NULL);
+   if ( surface == EGL_NO_SURFACE )
+   {
+      LOG("No surface...\n");
+      return EGL_FALSE;
+   }
+
+   // Create a GL context
+   context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs );
+   if ( context == EGL_NO_CONTEXT )
+   {
+      LOG("No context...\n");
+      return EGL_FALSE;
+   }
+
+   // Make the context current
+   if ( !eglMakeCurrent(display, surface, surface, context) )
+   {
+      LOG("Could not make the current window current !\n");
+      return EGL_FALSE;
+   }
+
+   ESContext.display = display;
+   ESContext.surface = surface;
+   ESContext.context = context;
+   return EGL_TRUE;
+}
+
+void shell_surface_ping
+(void *data, struct wl_shell_surface *shell_surface, uint32_t serial) {
+  wl_shell_surface_pong(shell_surface, serial);
+}
+
+void shell_surface_configure
+(void *data, struct wl_shell_surface *shell_surface, uint32_t edges,
+ int32_t width, int32_t height) {
+  struct window *window = data;
+  wl_egl_window_resize(ESContext.native_window, width, height, 0, 0);
+}
+
+void shell_surface_popup_done(void *data, struct wl_shell_surface *shell_surface) {
+}
+
+static struct wl_shell_surface_listener shell_surface_listener = {
+  &shell_surface_ping,
+  &shell_surface_configure,
+  &shell_surface_popup_done
+};
+
+EGLBoolean CreateWindowWithEGLContext(char *title, int width, int height) {
+  CreateNativeWindow(title, width, height);
+  return CreateEGLContext();
+}
+
+
+static void global_registry_handler
+(void *data, struct wl_registry *registry, uint32_t id,
+ const char *interface, uint32_t version) {
+  LOG("Got a registry event for %s id %d\n", interface, id);
+  if (strcmp(interface, "wl_compositor") == 0)
+    compositor =
+      wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+
+  else if (strcmp(interface, "wl_shell") == 0)
+    shell = wl_registry_bind(registry, id, &wl_shell_interface, 1);
+}
+
+static void global_registry_remover
+(void *data, struct wl_registry *registry, uint32_t id) {
+  LOG("Got a registry losing event for %d\n", id);
+}
+
+const struct wl_registry_listener listener = {
+  global_registry_handler,
+  global_registry_remover
+};
+
+static void
+get_server_references() {
+
+  struct wl_display * display = wl_display_connect(NULL);
+  if (display == NULL) {
+    LOG("Can't connect to wayland display !?\n");
+    exit(1);
+  }
+  LOG("Got a display !");
+
+  struct wl_registry *wl_registry =
+    wl_display_get_registry(display);
+  wl_registry_add_listener(wl_registry, &listener, NULL);
+
+  // This call the attached listener global_registry_handler
+  wl_display_dispatch(display);
+  wl_display_roundtrip(display);
+
+  // If at this point, global_registry_handler didn't set the
+  // compositor, nor the shell, bailout !
+  if (compositor == NULL || shell == NULL) {
+    LOG("No compositor !? No Shell !! There's NOTHING in here !\n");
+    exit(1);
+  }
+  else {
+    LOG("Okay, we got a compositor and a shell... That's something !\n");
+    ESContext.native_display = display;
+  }
+}
+
+void destroy_window() {
+  eglMakeCurrent(ESContext.display, NULL, NULL, NULL);
+  eglDestroySurface(ESContext.display, ESContext.surface);
+  wl_egl_window_destroy(ESContext.native_window);
+  wl_shell_surface_destroy(shell_surface);
+  wl_surface_destroy(surface);
+  eglDestroyContext(ESContext.display, ESContext.context);
+  eglTerminate(ESContext.display);
+}
+
+
+int main(int argc, char *argv[])
+{
+    // not referenced:
+    argc = argc;
+    argv = argv;
+
+    get_server_references();
+
+    surface = wl_compositor_create_surface(compositor);
+    if (surface == NULL) {
+        LOG("No Compositor surface ! Yay....\n");
+        exit(1);
+    }
+    else LOG("Got a compositor surface !\n");
+
+    shell_surface = wl_shell_get_shell_surface(shell, surface);
+    wl_shell_surface_set_toplevel(shell_surface);
+
+    if (!CreateWindowWithEGLContext(sAppName, sWindowWidth, sWindowHeight))
+    {
+        fprintf(stderr, "Graphics initialization failed.\n");
+        return EXIT_FAILURE;
+    }
+
+    appInit();
+
+    while (gAppAlive)
+    {
+        wl_display_dispatch_pending(ESContext.native_display);
+
+        if (gAppAlive)
+        {
+            struct timeval timeNow;
+
+            gettimeofday(&timeNow, NULL);
+            appRender(timeNow.tv_sec * 1000 + timeNow.tv_usec / 1000,
+                      sWindowWidth, sWindowHeight);
+            checkGLErrors();
+            eglSwapBuffers(sEglDisplay, sEglSurface);
+            checkEGLErrors();
+        }
+    }
+
+    appDeinit();
+    destroy_window();
+
+    return EXIT_SUCCESS;
+}

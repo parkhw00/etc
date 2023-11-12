49ee6abeeb0a2b70954b13f212ee8db727ca4f6c
Hyunwoo Park <hyunwoo.park@gmail.com>
2023-11-12 22:53:05 +0900

    serial-repeater: ^[ + q to quit

--- serial-repeater/serial-client.py
+++ serial-repeater/serial-client.py
@@ -10,28 +10,20 @@ import time
 import select
 import logging
 
-logger = logging.getLogger("serial-client")
+logger = logging.getLogger("serial-sock")
 
 
 class NonBlockingInput(object):
 
     def __enter__(self):
-        # canonical mode, no echo
         self.old = termios.tcgetattr(sys.stdin)
-        # self.old = tty.setraw(sys.stdin.fileno(), termios.TCSADRAIN)
-        # new = termios.tcgetattr(sys.stdin)
-        # new[3] = new[3] & ~(termios.ICANON | termios.ECHO)
-        # termios.tcsetattr(sys.stdin, termios.TCSADRAIN, new)
+        self.old = tty.setraw(sys.stdin.fileno(), termios.TCSADRAIN)
 
-        # set for non-blocking io
         self.orig_fl = fcntl.fcntl(sys.stdin, fcntl.F_GETFL)
         fcntl.fcntl(sys.stdin, fcntl.F_SETFL, self.orig_fl | os.O_NONBLOCK)
 
     def __exit__(self, *args):
-        # restore terminal to previous state
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old)
-
-        # restore original
         fcntl.fcntl(sys.stdin, fcntl.F_SETFL, self.orig_fl)
 
 
@@ -54,16 +46,27 @@ if __name__ == "__main__":
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    client.connect(opts.socket)
-    client.sendall(b"Hi...")
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(opts.socket)
+    sock.sendall(b"Hi...")
 
     with NonBlockingInput():
         ps = {}
 
+        got_special = False
+        running = True
         def f(data, event):
             if data:
-                client.sendall(data)
+                global got_special, running
+                if not got_special and data == b'\x1b':
+                    got_special = True
+                    logger.debug("got special key. ^[")
+                elif got_special and data == b'q':
+                    logger.debug("quit.")
+                    running = False
+                else:
+                    got_special = False
+                    sock.sendall(data)
         ps[sys.stdin.fileno()] = f
 
         def f(data, event):
@@ -71,18 +74,19 @@ if __name__ == "__main__":
                 sys.stdout.buffer.write(data)
             if event & select.POLLHUP:
                 raise Exception("Connection lost.")
-        ps[client.fileno()]    = f
+        ps[sock.fileno()] = f
 
         po = select.poll()
         for fd in ps:
             po.register(fd, select.POLLIN)
 
-        while True:
+        while running:
             es = po.poll()
             logger.debug(f"es: {es}")
             for fd, event in es:
+                data = None
                 if event & select.POLLIN:
                     data = os.read(fd, 1024)
-                    logger.debug(f"fd {fd}, event {event}, data {data}")
-                    ps[fd](data, event)
+                logger.debug(f"fd {fd}, event {event}, data {data}")
+                ps[fd](data, event)
 
